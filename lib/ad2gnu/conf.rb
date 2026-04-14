d0678c0bb09dc67918f6217ab79b4284d8b86a84
Pietro Donatini <pietro.donatini@unibo.it>
2026-04-14 15:43:07 +0200

    YAML.safe_load

--- lib/ad2gnu/conf.rb
+++ lib/ad2gnu/conf.rb
@@ -3,7 +3,7 @@ module AD2Gnu
     attr_reader :debug, :ldap, :ad, :account, :groups
 
     def initialize(file = "/etc/ad2gnu.yml")
-      @yaml = YAML.load(File.open(file))
+      @yaml = YAML.safe_load(File.read(file), permitted_classes: [], permitted_symbols: [], aliases: false)
       @debug = @yaml["debug"] || false
       @ldap = @yaml["ldap"]
       @ad = @yaml["ad"]
