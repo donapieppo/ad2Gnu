fd2b9bd352f6da60a6d6e3fe85d17e58c65cc60f
Pietro Donatini <pietro.donatini@unibo.it>
2026-04-14 16:15:33 +0200

    uid_number

--- bin/ad2gnu_add_user.rb
+++ bin/ad2gnu_add_user.rb
@@ -3,12 +3,12 @@
 require "ad2gnu"
 
 new_user = ARGV[0] or raise "Give user's login name (ex nome.cognome) or upn (with @unibo.it) or AD cn (ex Mat033845)"
-uidNumber = ARGV[1]
+uid_number = ARGV[1]
 
 ad2gnu = AD2Gnu::Base.new(:personale).ad_login.local_login
 
 user = ad2gnu.ad.get_user(new_user) 
 pp user
 
-options = uidNumber ? {uidNumber: uidNumber} : {}
+options = uid_number ? {uid_number: uid_number} : {}
 ad2gnu.local.add_user(user, options)
