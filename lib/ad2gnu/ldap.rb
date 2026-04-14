50293069f79eb10cd45f0958ab94cd5f36903708
Pietro Donatini <pietro.donatini@unibo.it>
2026-04-14 15:56:39 +0200

    clean

--- lib/ad2gnu/ldap.rb
+++ lib/ad2gnu/ldap.rb
@@ -6,10 +6,6 @@ class Ldap
     end
   end
 
-  def search_array(query)
-    @conn.search2(@base, LDAP::LDAP_SCOPE_SUBTREE, query)
-  end
-
   # supponendo univoci gli ou restituisce l'Ldap::Entry relativo all'ou
   def read_ou(ou)
     f = Net::LDAP::Filter.eq("ou", ou)
@@ -29,11 +25,5 @@ class Ldap
     nil
   end
   alias_method :leggi_cn, :read_cn
-
-  def search2(base, query)
-    @conn.search(base, LDAP::LDAP_SCOPE_SUBTREE, query) do |e|
-      yield e
-    end
-  end
 end
 end
